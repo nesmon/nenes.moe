3f72f97b6b1a56382021ae63cfebf42efe5d9145
nenes <nesmon2108@protonmail.com>
2025-02-18 21:30:10 +0100

    I still hate friontend but with @thomasbnt it's even more :sparkles: worst :sparkles: <3

--- pages/index.vue
+++ pages/index.vue
@@ -6,10 +6,10 @@
                 <div class="link-card">
                     <a href="https://www.youtube.com/@MaidNenes" target="_blank">
                         <div class="link-img">
-                            <img src="@/assets/img/youtube.svg" alt="Youtube">
+                            <Icon name="logos:youtube-icon" size="30" />
                         </div>
-                        <div class="vertical-line"></div>
-                        <div class="link-separator">
+                        <div class="link-separator"></div>
+                        <div class="link-content">
                             <p class="link-title">Youtube</p>
                         </div>
                     </a>
@@ -17,10 +17,10 @@
                 <div class="link-card">
                     <a href="https://www.instagram.com/maidnenes/" target="_blank">
                         <div class="link-img">
-                            <img src="@/assets/img/instagram.svg" alt="Instagram">
+                            <Icon name="skill-icons:instagram" size="30" />
                         </div>
-                        <div class="vertical-line"></div>
-                        <div class="link-separator">
+                        <div class="link-separator"></div>
+                        <div class="link-content">
                             <p class="link-title">Instagram</p>
                         </div>
                     </a>
@@ -28,10 +28,10 @@
                 <div class="link-card">
                     <a href="https://www.x.com/MaidNenes/" target="_blank">
                         <div class="link-img">
-                            <img src="@/assets/img/twitter.svg" alt="Twitter">
+                            <Icon name="fa6-brands:x-twitter" size="30" />
                         </div>
-                        <div class="vertical-line"></div>
-                        <div class="link-separator">
+                        <div class="link-separator"></div>
+                        <div class="link-content">
                             <p class="link-title">Twitter</p>
                         </div>
                     </a>
@@ -66,19 +66,19 @@
     flex-direction: column;
     align-items: center;
     gap: 10px;
+    margin: 0 45px;
 }
 
 .link-card {
     display: flex;
     align-items: center;
     justify-content: center;
-    width: 20%; 
-    min-height: 60px;
+    width: 25rem; 
     border-radius: 15px;
     border: 2px solid aquamarine;
     background: linear-gradient(90deg, rgba(245, 71, 230, 0.8) 0%, rgba(177, 115, 235, 0.8) 65%);
     padding: 5px;
-    transition: transform 0.3s ease, opacity 0.3s ease; /* Animation fluide */
+    transition: transform 0.3s ease, opacity 0.3s ease;
 }
 
 .link-card a {
@@ -90,10 +90,8 @@
     color: black;
 }
 
-.link-img img {
+.link-img {
     margin-left: 10px;
-    width: 40px;
-    height: 40px;
 }
 
 .link-content {
@@ -108,23 +106,20 @@
 }
 
 .link-separator {
-    width: 3px; /* Épaisseur de la ligne */
-    height: 45px; /* Hauteur de la ligne */
-    background-color: rgb(248, 188, 97); /* Couleur de la ligne */
+    width: 3px; 
+    height: 45px;
+    background-color: rgb(248, 188, 97); 
 }
 
-/* Effet au survol */
 .link-card:hover {
-    transform: scale(1.1); /* Agrandir la carte survolée */
+    transform: scale(1.1); 
 }
 
-/* Réduire les autres cartes */
 .links:hover .link-card {
     transform: scale(0.9);
     opacity: 0.7;
 }
 
-/* Réinitialiser l'effet sur la carte survolée */
 .links:hover .link-card:hover {
     transform: scale(1.1);
     opacity: 1;
